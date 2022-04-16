d0ef38d8e6d090448761cf87dfb41724032f313d
Rohit Sharma <rohitsharma@CFs-MacBook-Pro-167.local>
2022-04-16 20:34:30 +0530

    adding to github streamlit

--- cloud/streamlit_app.py
+++ cloud/streamlit_app.py
@@ -1,19 +1,9 @@
 import streamlit as st
 
 
-def sayHello():
-    st.write("hello world!")
-
-
 def app():
     st.markdown("## Data Upload")
 
     # Upload the dataset and save as csv
     st.markdown("### Upload a csv file for analysis.")
-    st.button('Say hello', on_click=sayHello)
-
-    name = st.text_input('Name')
-    if not name:
-        st.warning('Please input a name.')
-        st.stop()
-    st.success('Thank you for inputting a name.')
+    
